40fed74136c9e046ffcf0111fc8c182ed11fa982
Mark <49820822+Komaxor@users.noreply.github.com>
2026-04-25 12:00:50 +0200

    date

--- lib/config.ts
+++ lib/config.ts
@@ -1,5 +1,5 @@
 // April 2026 Heist Competition
-export const COMPETITION_START = new Date("2026-04-25T13:00:00Z"); // 15:00 CET
+export const COMPETITION_START = new Date("2026-04-25T11:00:00Z"); // 13:00 CET
 export const COMPETITION_LENGTH_MINUTES = 60;
 export const COMPETITION_END = new Date(COMPETITION_START.getTime() + COMPETITION_LENGTH_MINUTES * 60_000);
 
